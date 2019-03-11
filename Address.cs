0e51cc9ce2981d32c9607ff183f38063d534348a
gipson.hsu <Gipson.Hsu@quantatw.com>
2019-03-11 15:53:48 +0800

    initial

---
+++ Address.cs
@@ -0,0 +1,245 @@
+﻿using System.Diagnostics;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace GlobalAddressListSample
+{
+	[DebuggerDisplay("DisplayName = {DisplayName}, Mail = {Mail}")]
+	public class Address
+	{
+		internal Address(DirectoryEntry entry,int index,int size)
+		{
+			email = (string)entry.Properties["mail"].Value;
+			department = (string)entry.Properties["department"].Value;
+			company = (string)entry.Properties["company"].Value;
+			string lastName1 = (string)entry.Properties["sn"].Value;
+			string lastName2 = (string)entry.Properties["name"].Value;
+			if(email != null && email.Contains("quantacn")) {
+				//return;
+			}
+			if(email != null && email.Equals("Wenchi.Lan@quantatw.com")) {
+			}
+			if(company != null && !company.Contains("QCI") && !company.Contains("qci")) {
+			}
+
+			if(department != null && department.EndsWith("-QSMC")) {
+				//return;
+			}
+			object directReports = entry.Properties["directReports"].Value;
+			if(directReports != null) {
+				try {
+					object[] tmp;
+					if(directReports.GetType().FullName.Equals("System.String")) {
+						tmp = new object[1];
+						tmp[0] = directReports;
+					} else {
+						tmp = (object[])directReports;
+					}
+					reported = new Person[tmp.Length];
+					for (int i = 0; i < tmp.Length; i++) {
+						reported[i] = new Person();
+						string man = tmp[i] as string;
+						if(man.Contains(" (")) {
+							int l = man.IndexOf(" (");
+							int r = man.IndexOf(')');
+							reported[i].name = man.Substring(3, l - 3);
+							reported[i].chtName = man.Substring(l + 2, r - l - 2);
+						} else if(man.Contains(")(")) {
+							int l = man.IndexOf('(');
+							int m = man.IndexOf(")(");
+							int r = man.IndexOf("),");
+							reported[i].name = man.Substring(3, l - 3);
+							reported[i].chtName = man.Substring(l + 1, m - l - 1);
+							reported[i].alias = man.Substring(m + 2, r - m - 2);
+						} else if(!man.Contains("(") && !man.Contains(")")) {
+							reported[i].name = man.Substring(3, man.IndexOf(',') - 3);
+						} else if(man.Contains("(")) {
+							int l = man.IndexOf('(');
+							int r = man.IndexOf(')');
+							reported[i].name = man.Substring(3, l - 3);
+							reported[i].chtName = man.Substring(l + 1, r - l - 1);
+						} else {							
+						}
+					}
+				} catch {
+				}
+			}
+			number = (string)entry.Properties["telephoneNumber"].Value;
+			alias = (string)entry.Properties["mailNickname"].Value;
+			titleName = (string)entry.Properties["title"].Value;
+			if(company == null && titleName == null) {
+				return;
+			}
+			/*try {
+			if(company != null) {
+				foreach (string k in entry.Properties.PropertyNames) {
+					System.Console.WriteLine(k + " : " + entry.Properties[k].Value);
+				}
+			}
+			}catch{
+				
+			}*/
+			if(titleName == null) {
+				System.Console.WriteLine("[" + titleName + "][" + number + "][" + company + "][" + lastName1 + "]");
+				return;
+			}
+			int left = titleName.IndexOf('(');
+			int right = titleName.IndexOf(')');					
+			if(left == -1) {
+				if(titleName == null || number == null || company == null || lastName1 == null) {
+					System.Console.WriteLine("[" + titleName + "][" + number + "][" + company + "][" + lastName1 + "]");
+					return;
+				} else {
+					titleName = "不明";
+				}
+			}
+			name = (string)entry.Properties["title"].Value;
+			if(name.Contains(" ")) {
+				name = name.Substring(0, name.IndexOf(' '));
+			}
+			if(name.Contains("\u6052")) {
+				name = name.Replace("\u6052", "\u6046");
+			}
+			if(left >= 0 && right >= 0) {
+				titleName = titleName.Substring(left + 1, right - left - 1);
+			}
+			firstName = (string)entry.Properties["displayName"].Value;
+			if(firstName.Contains("(")) {
+				//System.Console.WriteLine(firstName);
+				if(firstName.IndexOf('(') == 0) {
+					firstName = (string)entry.Properties["sn"].Value;
+					firstName = firstName.Substring(0, firstName.IndexOf('.'));
+				} else {
+					firstName = firstName.Substring(0, firstName.IndexOf('('));
+				}
+			}
+			lastName = "";
+			if(lastName2.Contains("(")) {
+				lastName2 = lastName2.Substring(0, lastName2.LastIndexOf('(')).Trim(new char[]{ ' ' });
+			}
+			if(lastName2.Contains(" ")) {
+				lastName2 = lastName2.Substring(lastName2.LastIndexOf(' ') + 1);
+			}
+			if(lastName2.Contains(".")) {
+				lastName2 = lastName2.Substring(lastName2.LastIndexOf('.') + 1);
+			}
+			if(!lastName1.Equals(lastName2) && lastName2.Length > 0) {
+				lastName2 = lastName2.Substring(0, 1).ToUpper() + lastName2.Substring(1).ToLower();
+			}
+			if(!lastName1.Equals(lastName2)) {
+				if(name.Contains("鍾") || name.Contains("鐘")) {
+					lastName2 = lastName1;
+				}
+			}
+			if(firstName.Contains(" " + lastName1)) {
+				firstName = firstName.Replace(" " + lastName1, "");
+				lastName = lastName1;
+			} else if(firstName.Contains(lastName1 + " ")) {
+				firstName = firstName.Replace(lastName1 + " ", "");
+				lastName = lastName1;
+			} else if(firstName.Contains(" " + lastName2)) {
+				firstName = firstName.Replace(" " + lastName2, "");
+				lastName = lastName2;
+			} else if(firstName.Contains(lastName2 + " ")) {
+				firstName = firstName.Replace(lastName2 + " ", "");
+				lastName = lastName2;
+			}
+			firstName = firstName.Trim(new char[]{ ' ' });
+			if(name.Equals("\u9577\u5b89\u5f18\u52f3")) {
+				firstName = "Hiroisa";
+				lastName = "Nagayasu";
+			}
+			object obj = entry.Properties["msExchUMDtmfMap"];
+			office = (string)entry.Properties["physicalDeliveryOfficeName"].Value;
+			string manager = (string)entry.Properties["manager"].Value;
+			if(entry.Properties["thumbnailPhoto"].Value != null) {
+				//byte[] bytes = (byte[])entry.Properties["thumbnailPhoto"].Value;
+				//outputJpg(bytes, alias + ".jpg");
+			}
+			if(manager != null && manager.Contains(",")) {
+				manager = manager.Substring(3, manager.IndexOf(',') - 3);
+			} else {
+				/*foreach (string k in entry.Properties.PropertyNames) {
+					System.Console.WriteLine(k + " : " + entry.Properties[k].Value);
+				}*/
+				manager = "--";
+			}
+			boss = new Person();
+			if(manager.Contains("-")) {
+				manager = manager.Replace("-", " ");
+			}
+			if(manager.Contains("Yung Chang Chin")) {
+				manager = manager.Replace("Yung Chang Chin", "Y. C. Chin");
+			}
+			if(manager.Contains(" (") && manager.Contains(")")) {
+				int leftP = manager.IndexOf(" (");
+				int rightP = manager.IndexOf(')');
+				boss.name = manager.Substring(0, leftP).Replace(" ", "").Replace(".", "").ToLower();
+				boss.chtName = manager.Substring(leftP + 2, rightP - leftP - 2);
+			} else if(manager.Contains("(") && manager.Contains(")")) {
+				int leftP = manager.IndexOf('(');
+				int rightP = manager.IndexOf(')');
+				boss.name = manager.Substring(0, leftP).Replace(" ", "").Replace(".", "").ToLower();			
+				boss.chtName = manager.Substring(leftP + 1, rightP - leftP - 1);
+			} else if(manager != null) {
+				boss.name = manager.Replace(" ", "").Replace(".", "").ToLower();
+			}
+			if(boss.chtName != null && boss.name.Equals("at") && boss.chtName.Equals("\u8521\u80b2\u5b97")) {
+				boss.name = "attsai";
+			}
+			if(boss.chtName != null && boss.name.Equals("aaron") && boss.chtName.Equals("\u8b1d\u5fb7\u8cb4")) {
+				boss.name = "aaronhsieh";
+			}
+			if(boss.name.Equals("jwhwu") && boss.chtName == null) {
+				boss.name = "jeffreyhwu";
+			}
+			if(boss.chtName != null && boss.name.Equals("jrwenjeng") && boss.chtName.Equals("\u912d\u667a\u6587")) {
+				boss.name = "jwjeng";
+			}
+			if(boss.chtName != null && boss.name.Equals("vencentwcc") && boss.chtName.Equals("\u738b\u5efa\u660c")) {
+				boss.name = "vencentwang";
+			}
+			if(boss.chtName != null && boss.name.Equals("joeycchen") && boss.chtName.Equals("\u9673\u8000\u632f")) {
+				boss.name = "joechen";
+			}
+			if(name != null) {
+				string msg = "Processing " + index + " / " + size + " " + firstName + " " + lastName + "         ";
+				System.Console.Write(msg);
+				for (int i = 0; i < msg.Length; i++) {
+					System.Console.Write("\b");			
+				}
+			}
+		}
+		
+		public string alias;
+		public string number;
+		public string office;
+		public Person boss;
+		public Person[] reported;
+		public string email;
+		public string department;
+		public string company;
+		public string titleName;
+		public string lastName;
+		public string firstName;
+		public string name;
+
+		private static void outputJpg(byte[] data, string fileName)
+		{
+			using (Image image = Image.FromStream(new MemoryStream(data))) {
+				image.Save("D:\\jpg\\" + fileName, ImageFormat.Jpeg);
+			}
+		}
+	}
+	
+	public class Person
+	{
+		public string alias;
+		public string name;
+		public string chtName;
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Text;
+
+namespace GlobalAddressListSample
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			DateTime start = DateTime.Now;
+			Address[] addressList = GetGlobalAddressList();
+			var relationMap = new Dictionary<string, Person>(); // alias -> boss
+			var nameMap = new Dictionary<string, List<string>>(); // name -> alias
+			var titleMap = new Dictionary<string, string>(); // alias -> title
+			var addressMap = new Dictionary<string, Address>(); // name -> alias
+			foreach (Address item in addressList) {
+				if(item.alias != null && item.boss != null) {
+					relationMap.Add(item.alias, item.boss);
+					string newKey1 = item.firstName + " " + item.lastName + item.name;
+					string newKey2 = item.firstName + " " + item.lastName;
+					newKey1 = newKey1.Replace("-", " ").Replace(" ", "").Replace(".", "").ToLower();
+					newKey2 = newKey2.Replace("-", " ").Replace(" ", "").Replace(".", "").ToLower();
+					if(!nameMap.ContainsKey(newKey1)) {
+						var aliasList = new List<string>();
+						nameMap.Add(newKey1, aliasList);
+					}
+					if(!nameMap.ContainsKey(newKey2)) {
+						var aliasList = new List<string>();
+						nameMap.Add(newKey2, aliasList);
+					}
+					nameMap[newKey1].Add(item.alias);
+					nameMap[newKey2].Add(item.alias);
+					titleMap.Add(item.alias, item.titleName);
+					addressMap.Add(item.alias, item);
+				}
+			}
+			foreach (string item in relationMap.Keys) {
+				if(nameMap.ContainsKey(relationMap[item].name + relationMap[item].chtName)) {
+					var list = nameMap[relationMap[item].name + relationMap[item].chtName];
+					if(list.Count == 1) {
+						relationMap[item].alias = list[0];
+						continue;
+					} else {
+						foreach (string alias in list) { // make sure the same depart for relation
+							if(addressMap[alias].department.Equals(addressMap[item].department)) {
+								relationMap[item].alias = alias;
+								break;
+							}
+							if(addressMap[alias].office.Equals(addressMap[item].office)) {
+								relationMap[item].alias = alias;
+								break;
+							}
+							if(addressMap[item].department.Contains(addressMap[alias].department)) {
+								relationMap[item].alias = alias;
+								break;
+							}
+						}
+						if(relationMap[item].alias == null) {
+							foreach (string alias in list) { // make sure the same depart for relation
+								if(getWeight(titleMap[item]) < getWeight(titleMap[alias])) {
+									relationMap[item].alias = alias;
+									break;
+								}
+							}
+						} else {
+							continue;
+						}
+					}
+				}
+				if(nameMap.ContainsKey(relationMap[item].name)) {
+					var list = nameMap[relationMap[item].name];
+					if(list.Count == 1) {
+						relationMap[item].alias = list[0];
+						continue;
+					} else {
+						foreach (string alias in list) { // make sure the same depart for relation
+							if(addressMap[alias].department.Equals(addressMap[item].department)) {
+								relationMap[item].alias = alias;
+								break;
+							}
+							if(addressMap[alias].office.Equals(addressMap[item].office)) {
+								relationMap[item].alias = alias;
+								break;
+							}
+							if(addressMap[item].department.Contains(addressMap[alias].department)) {
+								relationMap[item].alias = alias;
+								break;
+							}
+						}
+						if(relationMap[item].alias == null) {
+							foreach (string alias in list) { // make sure the same depart for relation
+								if(getWeight(titleMap[item]) < getWeight(titleMap[alias])) {
+									relationMap[item].alias = alias;
+									break;
+								}
+							}
+						} else {
+							continue;
+						}
+					}
+				}
+			}
+			var finalList = new List<Address>();
+			foreach (Address item in addressList) {
+				if(item.email == null) {
+					continue;
+				}
+				if(item.alias == null) {
+					continue;
+				}
+				if(item.alias != null && relationMap.ContainsKey(item.alias) && relationMap[item.alias].alias != null) {
+					item.boss.alias = relationMap[item.alias].alias;
+				} else {
+					
+				}
+				finalList.Add(item);
+			}
+			outputList(finalList);
+			DateTime end = DateTime.Now;
+			System.Console.WriteLine(end.Subtract(start).TotalSeconds);
+			this.InitializeComponent();
+			Close();
+		}
+		
+		private static void outputList(List<Address> list)
+		{
+			list.Sort(delegate(Address a, Address b) {
+				if(a.alias == null && b.alias == null)
+					return 0;
+				else if(a.alias == null)
+					return -1;
+				else if(b.alias == null)
+					return 1;
+				else
+					return a.alias.CompareTo(b.alias);
+			});
+			DateTime now = DateTime.Now;
+			string fileName = "quanta_" + string.Format("{0:0000}{1:00}{2:00}{3:00}{4:00}", now.Year, now.Month, now.Day, now.Hour, now.Minute) + ".csv";
+			var sb = new StringBuilder();
+			foreach (Address item in list) {
+				if(item.number != null && item.name != null && item.firstName != null && item.firstName.Length > 0) {
+					sb.Append("\"" + item.alias + "\",");
+					sb.Append("\"" + item.email + "\",");
+					if(item.number.Length >= 3) {
+						sb.Append("\"" + item.number + "\",");
+					} else {
+						sb.Append("\"\",");
+					}
+					sb.Append("\"" + item.office + "\",");
+					sb.Append("\"" + (item.boss.alias??"--") + "\",");
+					sb.Append("\"" + item.firstName + "\",");
+					sb.Append("\"" + item.lastName + "\",");
+					sb.Append("\"" + item.titleName + "\",");
+					sb.Append("\"" + item.name + "\",");
+					sb.Append("\"" + item.department + "\"");
+					sb.AppendLine();
+				}
+			}
+			File.WriteAllText(fileName, sb.ToString());
+		}
+
+		public static Address[] GetGlobalAddressList()
+		{
+			using (var searcher = new DirectorySearcher()) {
+				using (var entry = new DirectoryEntry(searcher.SearchRoot.Path)) {
+					searcher.Filter = "(&(mailnickname=*)(objectClass=user))";
+					searcher.PropertiesToLoad.Add("cn");
+					searcher.PropertyNamesOnly = true;
+					searcher.SearchScope = SearchScope.Subtree;
+					searcher.Sort.Direction = SortDirection.Ascending;
+					searcher.Sort.PropertyName = "cn";
+					SearchResultCollection ret = searcher.FindAll();
+					int size = ret.Count;
+					int index = 0;
+					return ret.Cast<SearchResult>().Select(result => 
+						new Address(result.GetDirectoryEntry(), index++, size)
+					).ToArray();
+				}
+			}
+		}
+		
+		public static int getWeight(String t)
+		{
+			switch (t) {
+				case "董事長":
+					return 1000;
+				case "副董事長":
+					return 833;
+				case "CTO VP&GM":
+					return 666;
+				case "VP&GM":
+					return 666;
+				case "AVP&GM":
+					return 666;
+				case "Sr. VP&GM":
+					return 666;
+				case "資深副總經理":
+					return 583;
+				case "副總經理暨財務長":
+				case "執行副總經理":
+					return 533;
+				case "副總經理":
+					return 500;
+				case "協理":
+				case "技術協理":
+				case "專案協理":
+					return 417;
+				case "顧問":
+				case "顧問A":
+				case "顧問B":
+				case "處長":
+				case "技術處長":
+				case "專案處長":
+					return 367;
+				case "副處長":
+				case "專案副處長":
+				case "技術副處長":
+					return 333;
+				case "部經理":
+				case "專案部經理":
+				case "技術部經理":
+					return 300;
+				case "資深經理":
+				case "專案資深經理":
+				case "技術資深經理":
+					return 267;
+				case "經理":
+				case "專案經理":
+				case "技術經理":
+					return 233;
+				case "副理":
+				case "技術副理":
+				case "專案副理":
+					return 200;
+				case "一級專員":
+				case "資深組長":
+					return 167;
+				case "二級專員":
+				case "儲備幹部":
+					return 150;
+				case "工程師":
+				case "管理師":
+					return 133;
+				case "助理工程師":
+				case "助理管理師":
+				case "管理員":
+				case "組長":
+					return 100;
+				case "技術員":
+					return 83;
+				case "助理技術員":
+					return 67;
+				case "工讀生":
+					return 33;
+				default: 
+					return 100;
+			}
+		}
+	}
+}
